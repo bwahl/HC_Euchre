26c5dbc843b0258709c480c012c29320b3ac728b
bwahl <wahl@hanover.edu>
2018-06-04 17:05:24 -0400

    Add Suit and Value to Card class. Ref #1

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,46 @@
 public class Card {
+
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    enum Suit {
+        HEARTS("Hearts"),
+        DIAMONDS("Diamonds"),
+        CLUBS("Clubs"),
+        SPADES("Spades");
+
+        private final String suit;
+
+        Suit(String suit) {
+            this.suit = suit;
+        }
+
+        public String getSuit() {
+            return suit;
+        }
+    }
+
+    enum Value {
+        NINE("9"),
+        TEN("10"),
+        JACK("J"),
+        QUEEN("Q"),
+        KING("K"),
+        ACE("A");
+
+        private final String value;
+
+        Value(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
 }
---
+++ src/test/java/CardTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CardTest {
+
+    @Test
+    public void testConstructor() {
+        Card card;
+        card = new Card(Card.Suit.HEARTS, Card.Value.TEN);
+        Assert.assertNotNull(card);
+    }
+
+}
