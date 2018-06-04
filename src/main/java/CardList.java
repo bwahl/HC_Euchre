f833f63607d2dba4d0e39baf6124ddf5496bcd06
Theresa Wilson <theresaawilson@gmail.com>
2018-06-04 18:12:35 -0400

    Add buildFullDeck and addCard methods to CardList. ref #2

--- src/main/java/CardList.java
+++ src/main/java/CardList.java
@@ -9,7 +9,27 @@ public class CardList {
     cards = new ArrayList<>();
   }
 
+  public static CardList buildFullDeck() {
+
+    CardList fullDeck = new CardList();
+    for (Card.Suit suit : Card.Suit.values()) {
+      for (Card.Value value : Card.Value.values()) {
+        Card card = new Card(suit, value);
+        fullDeck.addCard(card);
+      }
+    }
+    return fullDeck;
+  }
+
+  private void addCard(Card card) {
+    cards.add(card);
+  }
+
   public boolean isEmpty() {
     return cards.isEmpty();
+  }
+
+  public int size() {
+    return cards.size();
   }
 }
--- src/test/java/CardListTest.java
+++ src/test/java/CardListTest.java
@@ -5,9 +5,17 @@ import static org.junit.Assert.*;
 
 public class CardListTest {
 
+  public static final int DECK_SIZE = 24;
+
   @Test
   public void testConstructor() {
     CardList cardList = new CardList();
     Assert.assertTrue("CardList should be empty", cardList.isEmpty());
+  }
+
+  @Test
+  public void testBuildFullDeck() {
+    CardList fullDeck = CardList.buildFullDeck();
+    Assert.assertEquals("Full deck should have 24 cards", DECK_SIZE, fullDeck.size());
   }
 }
