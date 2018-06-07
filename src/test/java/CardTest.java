cf398ad3cfbd11ce8bf8d17562171f429c42d29a
bwahl <wahl@hanover.edu>
2018-06-07 15:35:19 -0400

    Cleanup indent in CardTest.

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -3,11 +3,11 @@ import org.junit.Test;
 
 public class CardTest {
 
-    @Test
-    public void testConstructor() {
-        Card card;
-        card = new Card(Card.Suit.HEARTS, Card.Value.TEN);
-        Assert.assertNotNull(card);
-    }
+  @Test
+  public void testConstructor() {
+    Card card;
+    card = new Card(Card.Suit.HEARTS, Card.Value.TEN);
+    Assert.assertNotNull(card);
+  }
 
 }
