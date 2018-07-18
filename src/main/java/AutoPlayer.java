8a60eef36191356004f50bc4f24e547596a5a329
bwahl <wahl@hanover.edu>
2018-07-18 15:39:17 -0400

    Add AutoPlayer constructor.

--- src/main/java/AutoPlayer.java
+++ src/main/java/AutoPlayer.java
@@ -1,4 +1,9 @@
 public class AutoPlayer extends Player {
+  public AutoPlayer() {
+    super();
+    setHand(new CardList());
+  }
+
   @Override
   Card playCard(Trick trick) {
     return this.getHand().drawCard();
